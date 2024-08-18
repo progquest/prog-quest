2ff99eb7454a555d899bcf7e73f35d5e026d9cba
kodiitulip <kodii.tulip@gmail.com>
2024-08-18 01:35:28 -0300

    reorganização do LoginForm e aplicação de css

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,11 +1,38 @@
 import LoginForm from "@/components/login-form/LoginForm";
+import Image from "next/image";
+// "./public/assets/icons/login-page-icon/icon.svg"
 import React from "react";
 
 const Login = () => {
 	return (
-		<div className='flex justify-center items-center min-h-screen bg-[#D9D9D9]'>
-			<LoginForm />
-		</div>
+		<>
+			{/* Logo Icon */}
+			<div className='absolute top-8 left-8 flex flex-row items-center m-2.5'>
+				<Image
+					src={"./assets/icons/login-page-icon/logo-icon.svg"}
+					alt='logo'
+					width={115}
+					height={90}
+					className=''
+				/>
+				<h1 className='text-4xl font-bold text-bpurple'>ProgQuest</h1>
+			</div>
+			{/* Register Button */}
+			<button
+				className=' absolute top-8 right-8
+                        flex flex-row justify-center items-center
+                        bg-bpurple shadow-def-button 
+                        rounded-full m-2.5 text-white text-2xl 
+                        font-semibold px-8 py-2.5'
+			>
+				Registrar
+			</button>
+
+			{/* Login Form */}
+			<div className='flex flex-col justify-center items-center min-h-screen bg-bgray'>
+				<LoginForm />
+			</div>
+		</>
 	);
 };
 
--- components/login-form/LoginForm.jsx
+++ components/login-form/LoginForm.jsx
@@ -1,47 +1,106 @@
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
-import { FaLock, FaUser } from "react-icons/fa";
+import { FaGithub, FaGoogle, FaLock, FaUser } from "react-icons/fa";
 
+/**
+ * Um quadro com forms de login para credenciais e OAuth
+ *
+ * Nesse quadro conta com caixas de texto para as credenciais do usuário
+ * e botões para logar com Google ou Github
+ *
+ * TODO: Fazer a autenticação funcional
+ * @returns {React.JSX.Element}
+ */
 const LoginForm = () => {
 	return (
-		<div className='w-md bg-transparent text-white'>
+		<div className='w-1/3 bg-bbox-rect text-black p-12 rounded-xl border-2 border-bbox-border'>
+			<Image
+				src={"/assets/icons/login-page-icon/hide-chameleon.svg"}
+				width={230}
+				height={150}
+				className='absolute left-[calc(50%-115px)] top-[90px]'
+			/>
+
 			<form action=''>
 				<h1 className='text-4xl text-center'>Login</h1>
 
+				{/* Usuário */}
 				<div className='relative w-full h-1/2 my-[30px] mx-0'>
 					<input
-						className='w-full h-full bg-tranparent border-none
-                    outline-2 outline-[rgba(255,255,255,.2)] rounded-lg'
+						className='w-full h-full bg-bgray rounded-full
+                    placeholder-shown:text-gray-500 text-gray-500 p-5
+                    vaild:border-green-500 invalid:border-red-500'
 						type='text'
-						placeholder='Username'
+						placeholder='Usuário'
 						required
 					/>
-					<FaUser />
-				</div>
-				<div className='relative w-full h-1/2 my-[30px] mx-0'>
-					<input
-						className='w-full h-full bg-tranparent border-none
-                    outline-2 outline-[rgba(255,255,255,.2)] rounded-lg'
-						type='password'
-						placeholder='Password'
-						required
-					/>
-					<FaLock />
-				</div>
-				<div className=''>
-					<label>
-						<input type='checkbox' />
-						Lembre-se de mim
-					</label>
-					<Link href={"#"}>Esqueceu a senha?</Link>
+					<FaUser className='absolute right-5 top-1/2 -translate-y-1/2 text-base' />
 				</div>
 
-				<button type='submit'>Entrar</button>
-				<div className=''>
-					<p>
-						Não tem uma conta?{" "}
-						<Link href={"/register"}>Cadastrar</Link>
-					</p>
+				{/* Senha */}
+				<div className='relative w-full h-1/2 my-[30px] mx-0'>
+					<input
+						className='w-full h-full bg-bgray rounded-full
+                    placeholder-shown:text-gray-500 text-gray-500 p-5
+                    vaild:border-green-500 invalid:border-red-500'
+						type='password'
+						placeholder='Senha'
+						required
+					/>
+					<FaLock className='absolute right-5 top-1/2 -translate-y-1/2 text-base' />
+				</div>
+
+				{/* Esqueceu a senha? */}
+				<div className='flex flex-col'>
+					<Link
+						href={"#"}
+						className='text-[#525252] text-right flex-grow mb-2'
+					>
+						Esqueceu a senha?
+					</Link>
+				</div>
+
+				{/* Botões */}
+				<div className='flex flex-col justify-center'>
+					{/* Entrar */}
+					<button
+						type='submit'
+						className='
+                        justify-center items-center
+                        p-2.5 bg-bpurple shadow-def-button 
+                        rounded-full flex-grow m-2.5 text-white text-2xl font-semibold'
+					>
+						Entrar
+					</button>
+					<hr className='bg-black mt-2.5' />
+
+					{/* OAuth */}
+					<div className='flex flex-row w-full justify-between'>
+						{/* Google Login */}
+						<button
+							className='
+                        flex flex-row justify-center items-center
+                        p-2.5 gap-2.5 bg-bpurple shadow-def-button 
+                        rounded-full flex-grow m-2.5 text-white text-2xl 
+                        font-semibold mouse-pointer'
+						>
+							<FaGoogle />
+							Google
+						</button>
+
+						{/* Github Login */}
+						<button
+							className='
+                        flex flex-row justify-center items-center
+                        p-2.5 gap-2.5 bg-bpurple shadow-def-button 
+                        rounded-full flex-grow m-2.5 text-white text-2xl 
+                        font-semibold mouse-pointer'
+						>
+							<FaGithub />
+							Github
+						</button>
+					</div>
 				</div>
 			</form>
 		</div>
